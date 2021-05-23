2e81d148e251811f513c6484bc4ae368fc902f85
Abdul Hakam bin Adli <hakamadli06@gmail.com>
2021-05-24 01:00:02 +0800

    init

---
+++ android/app/src/main/kotlin/com/example/cwc_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cwc_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:cwc_flutter/gmap.dart';
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Abdul Hakam bin Adli 204234',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: MyHomePage(title: 'Abdul Hakam bin Adli 204234'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    _getLocationPermission();
+  }
+
+  void _getLocationPermission() async {
+    var location = new Location();
+    try {
+      location.requestPermission();
+    } on Exception catch (_) {
+      print('There was a problem allowing location access');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Google Maps in Flutter',
+                style: TextStyle(fontSize: 42),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'SSE3151 - Lab 7: This is a new Flutter App using Google Maps created by Abdul Hakam bin Adli 204234.',
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Increment',
+        child: Icon(Icons.map),
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => GMap()),
+        ),
+      ),
+    );
+  }
+}
